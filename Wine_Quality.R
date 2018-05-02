9ac19d645877c4ed6de956046bbaf9be95ca48c5
es3573 <34548451+es3573@users.noreply.github.com>
2018-05-02 19:51:38 -0400

    adjusting train() parameters

--- Wine_Quality.R
+++ Wine_Quality.R
@@ -11,9 +11,12 @@ white_wines[,(1:11)] <- scale(white_wines[,(1:11)])
 
 # 5-fold cross-validation with 20 runs
 require(caret)
-train_control <-trainControl(method = "repeatedcv", number = 5, repeats = 20)
+train_control <-trainControl(method = "repeatedcv", number = 5, repeats = 20, verboseIter = TRUE)
 
-# Multiple Regression (error: invalid subscript type 'language' - not sure what this means)
-reds_MR <- train(formula = quality ~ ., data = red_wines, trControl = train_control, method = "lm")
-whites_MR <- train(formula = quality ~ ., data = white_wines, trControl = train_control, method = "lm")
+# Multiple Regression 
+reds_MR <- train(quality ~ ., data = red_wines, method = "lm", trControl = train_control)
+whites_MR <- train(quality ~ ., data = white_wines, method = "lm", trControl = train_control)
 
+# nnet 
+reds_nnet <-train(quality ~ ., data = red_wines, method = "nnet", trControl = train_control)
+whites_nnet <-train(quality ~ . , data = white_wines, method = "nnet", trControl = train_control)
