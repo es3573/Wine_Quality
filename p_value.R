3fdd4c76c2bad9a98c4f1a873717c506368eb85d
paulxiongfree <xzr0708@gmail.comn>
2018-05-08 15:47:43 -0400

    modify p val

--- p_value.R
+++ p_value.R
@@ -30,10 +30,10 @@ colnames(white_wines)[order(white_p_value)]
 
 
 red_imp  = sort(log(red_p_value))
-red_bp = barplot(red_imp[11:1], main="red Wine", horiz=TRUE, col = 'white')
+red_bp = barplot(red_imp[11:1], main="red Wine", horiz=TRUE, col = 'white', xlab = 'logspace of p-value', ylab = 'features')
 text(x=red_imp[11:1], y = red_bp, labels=colnames(red_wines)[order(log(red_p_value))][11:1], cex = 0.6, xpd = TRUE, pos = 2)
 
 white_imp  = sort(log(white_p_value))
-white_bp = barplot(white_imp[11:1], main="white Wine", horiz=TRUE, col = 'white')
+white_bp = barplot(white_imp[11:1], main="white Wine", horiz=TRUE, col = 'white', xlab = 'logspace of p-value', ylab = 'features')
 text(x=white_imp[11:1], y = white_bp, labels=colnames(white_wines)[order(log(white_p_value))][11:1], cex = 0.6, xpd = TRUE, pos = 2)
 
